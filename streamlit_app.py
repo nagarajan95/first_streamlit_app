46a03638c0624f5df3c3646b28802ea2d9691f73
nagarajan95 <l.nagarajan95@gmail.com>
2023-01-18 21:09:50 +0530

    Update streamlit_app.py
    added the space

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,6 @@ streamlit.header("The Fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.text("Thanks for adding" + add_my_fruit)
+streamlit.text("Thanks for adding " + add_my_fruit)
 
 
