bef3e003e3455a7b3d0fe953eeb8dce79bd417ef
nagarajan95 <l.nagarajan95@gmail.com>
2023-01-18 21:24:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
-
+import snowflake.connector
+import requests
+from urllib.error import URLError
 streamlit.title("My parents new healthy Diner")
 
 streamlit.header('Breakfast Menu')
@@ -22,22 +24,30 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 
 streamlit.dataframe(fruits_to_show)
-
+try:
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  
+  streamlit.error('Please select a fruit to get information")
+else:
+                  
+                  
+                  
+                  
+                  streamlit.write('The user entered ', fruit_choice)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+                  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+                  # write your own comment -what does the next line do? 
+                  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+                  # write your own comment - what does this do?
+                  streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
-
+except URLError as e:
+                   streamlit.error()
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -46,6 +56,7 @@ streamlit.header("The Fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.text("Thanks for adding " + add_my_fruit)
+streamlit.text("Thanks for adding ", add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')") ,
 
 
